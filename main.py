6a32cb88d2e81b0b23b6dfd328ae7212a64b0dde
kmerchant1 <kaiden.merchant@gmail.com>
2023-08-01 12:55:16 -0700

    defined function to stream live webcam to streamlit.

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 import streamlit as st
+import cv2
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+def run_live_feed():
+
+    cap = cv2.VideoCapture(0)
+    frame_placeholder = st.empty()
+    stop = st.button('Stop')
+
+    while True:
+        if stop:
+            break
+        ret, frame = cap.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_placeholder.image(frame, channels='RGB')
+
+        
+
+    cap.release()
+    cv2.destroyAllWindows()
